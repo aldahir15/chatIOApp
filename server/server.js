95b199818b7b5ba82b13254fe1dd97ded46bfe38
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-12-24 19:26:57 -0800

    Add custom methods for messages

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'Jon',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('disconnected USER');
   });
